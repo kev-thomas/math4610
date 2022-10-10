2246fdc18904d47d23e1570510fe3985020e2b6a
kev-thomas <ktomas1713@gmail.com>
2022-10-09 19:08:19 -0600

    begin assn3

---
+++ assn3/bisection.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+double bisection(double (*f)(double), double a, double b, double tol) {
+    double maxIter = -((log(tol) / log(b - a)) / log(2)) + 1
+    int x;
+    int i = 0;
+
+    while(i < maxIter) {
+        x = (a + b) / 2
+
+        if(f(x) == 0 || (b - a) / 2 < tol) {
+            return x;
+        }
+
+        if(x < 0) {
+            a = x;
+        }
+        else {
+            b = x;
+        }
+        i += 1;
+
+    }
+
+    return x;
+
+}
